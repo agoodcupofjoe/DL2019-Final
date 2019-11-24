1b32f44f2c397f3979a4dc2cf99039af79b51211
edimen <elizabeth_dimen@brown.edu>
2019-11-24 16:16:54 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import tensorflow as tf 
+import numpy as np 
+
+def train(model, train_inputs, train_labels):
+
+	
+	
+
+
+def test(model, test_inputs, test_labels):
+	pass
+
+def main():
+	pass
+
+if __name__ = '__main__':
+	main()
