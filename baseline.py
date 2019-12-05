c010dc3ed5c2f0cc5f316e3d5154b3903ff986e4
agoodcupofjoe <57635284+agoodcupofjoe@users.noreply.github.com>
2019-12-05 18:23:30 -0500

    Add files via upload

--- baseline.py
+++ baseline.py
@@ -5,48 +5,64 @@ class Baseline(tf.keras.Model):
 
         super(Baseline,self).__init__()
 
-        self.batch_size = None
-        
+        self.batch_size = 500
+
         self.num_classes = 2
+        self.num_epochs = 15
         self.image_height = 300
         self.image_width = 400
-        
+
         self.C1 = tf.keras.layers.Conv2D(16,5,padding='SAME',strides=2,kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1),bias_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1))
-        self.B1 = tf.keras.layers.BatchNormalization()
+        self.B1 = tf.keras.layers.BatchNormalization(name='bn1')
         self.P1 = tf.keras.layers.MaxPool2D(pool_size=3,strides=2,padding='VALID')
+
         self.C2 = tf.keras.layers.Conv2D(20,5,padding='SAME',strides=1,kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1),bias_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1))
-        self.B2 = tf.keras.layers.BatchNormalization()
+        self.B2 = tf.keras.layers.BatchNormalization(name='bn2')
         self.P2 = tf.keras.layers.MaxPool2D(pool_size=2,strides=2,padding='VALID')
+
         self.C3 = tf.keras.layers.Conv2D(20,5,padding='SAME',strides=1,kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1),bias_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1))
-        self.B3 = tf.keras.layers.BatchNormalization()
+        self.B3 = tf.keras.layers.BatchNormalization(name='bn3')
+        self.flatten = tf.keras.layers.Flatten()
+
         self.F1 = tf.keras.layers.Dense(320,activation='relu',kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1),bias_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1))
         self.D1 = tf.keras.layers.Dropout(0.3)
+
         self.F2 = tf.keras.layers.Dense(160,activation='relu',kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1),bias_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1))
         self.D2 = tf.keras.layers.Dropout(0.3)
-        self.F3 = tf.keras.layers.Dense(40,kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1),bias_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1))
-        
+
+        self.F3 = tf.keras.layers.Dense(self.num_classes,kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1),bias_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1))
+
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
     @tf.function
     def call(self,inputs,training):
+        # First convolution layer
         conv1 = self.C1(inputs)
         relu1 = tf.nn.relu(conv1)
         batchnorm1 = self.B1(relu1,training)
-        maxpool1 = self.P1(relu1)
+        maxpool1 = self.P1(batchnorm1)
+
+        # Second convolution layer
         conv2 = self.C2(maxpool1)
         relu2 = tf.nn.relu(conv2)
         batchnorm2 = self.B2(relu2,training)
-        maxpool2 = self.P2(relu2)
+        maxpool2 = self.P2(batchnorm2)
+
+        # Third convolution layer
         conv3 = self.C3(maxpool2)
         relu3 = tf.nn.relu(conv3)
         batchnorm3 = self.B3(relu3,training)
-        dense1 = self.D1(self.F1(batchnorm3),training)
+
+        # Sequence of dense layers to produce label prediction
+        dense1 = self.D1(self.F1(self.flatten(batchnorm3)),training)
         dense2 = self.D2(self.F2(dense1),training)
         dense3 = self.F3(dense2)
+
+        # Return the output of the final dense layer
         return dense3
 
     def loss(self,logits,labels):
         return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels,logits))
 
     def accuracy(self,logits,labels):
-        return tf.reduce_mean(tf.cast(tf.equal(tf.argmax(logits,1),tf.argmax(labels,1))))
+        return tf.reduce_mean(tf.cast(tf.equal(tf.argmax(logits,1),tf.argmax(labels,1)), dtype=tf.float32))
--- main.py
+++ main.py
@@ -8,35 +8,46 @@ import glob
 
 def train(model, train_inputs, train_labels):
 	# Random shuffling of data
-	rand_indx = tf.random.shuffle(np.arange(len(model.batch_size)))
+	rand_indx = tf.random.shuffle(np.arange(model.batch_size))
 	train_inputs, train_labels = tf.gather(train_inputs, rand_indx), tf.gather(train_labels, rand_indx)
 
 	# Dividing data into 5 blocks, doing different transform on each (one get no transform)
 	block_size = model.batch_size // 5
 	datagen = tf.keras.preprocessing.image.ImageDataGenerator().apply_transform
 
+	transformed_jpegs = []
+
 	# Left right flip
-	train_inputs[0:block_size,:,:,:] = [datagen(x, {'flip_horizontal':True}) for x in train_inputs[0:block_size,:,:,:]]
+	for index in range(0, block_size):
+		transformed_jpegs.append(datagen(train_inputs[index,:,:,:], {'flip_horizontal':True}))
 
 	# Up down flip
-	train_inputs[block_size:2*block_size,:,:,:] = [datagen(x, {'flip_vertical':True}) for x in train_inputs[block_size:2*block_size,:,:,:]]
+	for index in range(block_size, 2*block_size):
+		transformed_jpegs.append(datagen(train_inputs[index,:,:,:], {'flip_vertical':True}))
 
 	# Random rotation
-	train_inputs[2*block_size:3*block_size,:,:,:] = [datagen(x, {'theta':float(np.random.randint(360))}) for x in train_inputs[2*block_size:4*block_size,:,:,:]]
+	for index in range(2*block_size, 3*block_size):
+		transformed_jpegs.append(datagen(np.array(train_inputs[index,:,:,:]), {'theta':float(np.random.randint(360))}))
 
 	# Random shear between 0 and 20 degrees
-	train_inputs[3*block_size:4*block_size,:,:,:] = [datagen(x, {'shear':float(np.random.randint(20))}) for x in train_inputs[3*block_size:4*block_size,:,:,:]]
+	for index in range(3*block_size, 4*block_size):
+		transformed_jpegs.append(datagen(np.array(train_inputs[index,:,:,:]), {'shear':float(np.random.randint(20))}))
 
-	for img, label in zip(train_inputs, train_labels):
-		with tf.GradientTape() as tape:
-			logits = model.call(img)
-			loss = model.loss(logits, label)
-		gradients = tape.gradient(loss, model.trainable_variables)
-		model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+	for index in range(4*block_size, model.batch_size):
+		transformed_jpegs.append(train_inputs[index,:,:,:])
+
+	new_train_inputs = tf.stack(transformed_jpegs)
+
+	with tf.GradientTape() as tape:
+		logits = model.call(new_train_inputs, True)
+		loss = model.loss(logits, train_labels)
+
+	gradients = tape.gradient(loss, model.trainable_variables)
+	model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
 def test(model, test_inputs, test_labels):
 	# Pass the test images through the model's call function
-	logits = model.call(test_inputs)
+	logits = model.call(test_inputs, False)
 
 	# Determine the accuracy using the outputted logits and predictions
 	accuracy = model.accuracy(logits, test_labels)
@@ -60,25 +71,31 @@ def main():
 	train_data = tf.convert_to_tensor(train_data, dtype=tf.float32)
 
 	# Acquire the labels for the corresponding training images
-	train_labels = get_one_hots_diagnosis("processed_data/train/labels/*")
+	train_labels = get_one_hots_diagnosis("processed_data/train/meta/*")
 
 	# Acquire and load testing images as tensors
 	test_data = load_jpegs("processed_data/test/img/*.jpeg")
 	test_data = tf.convert_to_tensor(test_data, dtype=tf.float32)
 
 	# Acquire the labels for the corresponding testing images
-	test_labels = get_one_hots_diagnosis("process_data/test/labels/*")
+	test_labels = get_one_hots_diagnosis("processed_data/test/meta/*")
 
 	# Construct baseline model
 	baseline_model = Baseline()
 
 	# Determine number of training images
 	num_train = tf.shape(train_data)[0]
+	indices = tf.Variable(np.arange(0, num_train, 1))
 
 	# Train the baseline model for the following number of epochs
 	for epoch_index in range(baseline_model.num_epochs):
 		# Determine the number of batches to run and train
 		num_batches = (num_train // baseline_model.batch_size)
+		rand_indices = tf.random.shuffle(indices)
+
+		# Shuffle the inputs and the labels using the shuffled indices
+		train_inputs = tf.gather(train_data, rand_indices)
+		train_labels = tf.gather(train_labels, rand_indices)
 
 		for batch_index in range(num_batches):
 			# Determine the indices of the images for the current batch
@@ -96,7 +113,8 @@ def main():
 	baseline_accuracy = test(baseline_model, test_data, test_labels) * 100
 
 	# Print out the accuracy of the baseline model
-	print(f"Baseline Model Accuracy: {baseline_accuracy}%")
+	# print(f"Baseline Model Accuracy: {baseline_accuracy}%")
+	print(baseline_accuracy)
 
 
 
